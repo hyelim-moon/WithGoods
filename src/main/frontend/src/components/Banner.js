8631f47dc2b58cf4b5ec0b21238c00426af4270f
dus <yeondy017@kyungmin.ac.kr>
2025-05-14 09:53:20 +0900

    다연 Navbar 테두리 색상 변경

--- src/main/frontend/src/components/Banner.js
+++ src/main/frontend/src/components/Banner.js
@@ -5,7 +5,7 @@ import styles from '../assets/styles/Banner.module.css';
 function Banner() {
     return (
         <section className={styles.banner}>
-            <h2>HAPPINESS WITH PIT A PET</h2>
+            <h2></h2>
         </section>
     );
 }
