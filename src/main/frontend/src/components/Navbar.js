88cb85fcea9614d8548c3fc592a8349212bae0f4
문혜림 <mhl0612@kyungmin.ac.kr>
2025-05-14 09:46:18 +0900

    네비 프론트 수정

--- src/main/frontend/src/components/Navbar.js
+++ src/main/frontend/src/components/Navbar.js
@@ -1,14 +1,16 @@
-import React from 'react';
-import styles from '../assets/styles/Navbar.css';
-
-const categories = ['NEW', 'SALE', 'PUPPY', 'ADULT', 'SENIOR', 'FOOD', 'SNACK', 'TOY', 'SUPPLIES'];
+import { Link } from 'react-router-dom';
+import styles from '../assets/styles/Navbar.module.css';
 
 function Navbar() {
     return (
         <nav className={styles.navbar}>
-            {categories.map(cat => (
-                <span key={cat}>{cat}</span>
-            ))}
+            <Link to="/best"><button>⭐BEST⭐</button></Link>
+            <Link to="/all"><button>전체</button></Link>
+            <Link to="/anniversary"><button>기념일</button></Link>
+            <Link to="/customization"><button>커스텀굿즈</button></Link>
+            <Link to="/limited_edition"><button>한정판</button></Link>
+            <Link to="/inquiry"><button>문의</button></Link>
+            <Link to="/registration"><button>상품등록</button></Link>
         </nav>
     );
 }
