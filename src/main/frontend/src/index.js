e6fd63b85d20c7a73be94259922a7aa839e07676
dus <yeondy017@kyungmin.ac.kr>
2025-05-21 11:37:59 +0900

    다연 장바구니 수정 중

--- src/main/frontend/src/index.js
+++ src/main/frontend/src/index.js
@@ -1,14 +1,14 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import './assets/styles/global.module.css';
+import './assets/styles/Global.module.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
+    <React.StrictMode>
+        <App />
+    </React.StrictMode>
 );
 
 // If you want to start measuring performance in your app, pass a function
