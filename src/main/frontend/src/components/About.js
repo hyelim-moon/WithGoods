959e9ec925b1c60e2b052924be055485841dad3e
hyeon-3 <tjrgus3020@naver.com>
2025-05-27 15:29:12 +0900

    back 및 front 분리 및 백환경수정

---
+++ src/main/frontend/src/components/About.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+function About() {
+    return (
+        <div>
+            <h1>소개</h1>
+            <p>이 프로젝트는 React 프론트엔드와 Spring Boot 백엔드로 구성되어 있습니다.</p>
+        </div>
+    );
+}
+
+export default About;
---
+++ src/main/frontend/src/components/Home.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+function Home() {
+    return (
+        <div>
+            <h1>홈페이지</h1>
+            <p>React와 Spring Boot 연동 프로젝트입니다.</p>
+        </div>
+    );
+}
+
+export default Home;
