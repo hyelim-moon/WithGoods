4a024249bf624ef14b1bd5ecef56757f77e5c1ff
yeon171 <yeondy01@naver.com>
2025-05-28 14:33:05 +0900

    다연 등록된 상품 페이지 완

--- src/main/frontend/src/App.js
+++ src/main/frontend/src/App.js
@@ -22,6 +22,7 @@ import ProductDetail from './components/ProductDetail';
 import MyPage from './components/MyPage';
 import EditProfile from "./components/EditProfile";
 import Registration from "./components/Registration";
+import MyProductList from "./components/MyProductList";
 
 function Record() {
     return null;
@@ -65,6 +66,7 @@ function App() {
                     <Route path="/inquiry/write" element={<InquiryForm />} />
                     <Route path="/inquiry/estimate" element={<EstimateInquiryForm />} />
                     <Route path="/mypage" element={<MyPage />} />
+                    <Route path="/myproductlist" element={<MyProductList />} />
                     <Route path="/cart" element={<Cart />} />
                     <Route path="/product/:id" element={<ProductDetail />} />
                     <Route path="/edit-profile" element={<EditProfile />} />
