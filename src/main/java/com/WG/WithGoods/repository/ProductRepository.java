170d77174d7d91084cfcc90ac32d40bdeb4660fe
tjdud03 <wjstjdud96@naver.com>
2025-06-17 17:07:30 +0900

    검색시 제목검색 우선정렬

--- src/main/java/com/WG/WithGoods/repository/ProductRepository.java
+++ src/main/java/com/WG/WithGoods/repository/ProductRepository.java
@@ -21,8 +21,6 @@ public interface ProductRepository extends JpaRepository<Product, Integer> {
 
     List<Product> findByRole(ProductRole role);
 
-    List<Product> findByNameContainingIgnoreCaseOrDescriptionContainingIgnoreCase(String nameKeyword, String descriptionKeyword);
-
     List<Product> findByNameContainingIgnoreCase(String name);
 
     List<Product> findByDescriptionContainingIgnoreCase(String description);
